c64b157a7e147c3ffbf7aab082cd69982fd64a58
Evokoo <evanjohncooper@gmail.com>
2025-01-12 17:27:11 +1100

    Quest 05 created

---
+++ 05/05.ts
@@ -0,0 +1,10 @@
+// Imports
+import Utils from "Utils";
+
+//Solution
+export default function solve(fileName: string, quest: string): number {
+	const data = Utils.readData(fileName, quest);
+	return 0;
+}
+
+// Functions
