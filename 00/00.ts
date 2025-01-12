0d89abee697a53a2c52cffeed55146c21ad55171
Evokoo <evanjohncooper@gmail.com>
2025-01-12 17:09:32 +1100

    Updated template

--- 00/00.ts
+++ 00/00.ts
@@ -1,7 +1,7 @@
 // Imports
 import Utils from "Utils";
 
-//Part 1
+//Solution
 export default function solve(fileName: string, quest: string): number {
 	const data = Utils.readData(fileName, quest);
 	return 0;
