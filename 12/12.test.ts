7a300fcbeed93cd016bdfe3b1d537c265a70544d
Evokoo <evanjohncooper@gmail.com>
2025-01-22 17:38:43 +1100

    Quest 12 - Part 2 complete

---
+++ 12/12.test.ts
@@ -0,0 +1,38 @@
+import * as path from "@std/path";
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import solve from "./12.ts";
+
+const currentQuest = path.basename(Deno.cwd());
+
+describe(`EBC 2014 - Quest ${currentQuest}`, () => {
+	describe("I", () => {
+		it("Example", () => {
+			expect(solve("example_I", currentQuest)).toBe(13);
+		});
+
+		it("Solution", () => {
+			expect(solve("input_I", currentQuest)).toBe(243);
+		});
+	});
+
+	describe("II", () => {
+		it("Example", () => {
+			expect(solve("example_II", currentQuest)).toBe(22);
+		});
+
+		it("Solution", () => {
+			expect(solve("input_II", currentQuest)).toBe(20669);
+		});
+	});
+
+	describe.skip("III", () => {
+		it("Example", () => {
+			expect(solve("example_III", currentQuest)).toBe(0);
+		});
+
+		it.skip("Solution", () => {
+			expect(solve("input_III", currentQuest)).toBe(0);
+		});
+	});
+});
---
+++ 12/12.ts
@@ -0,0 +1,83 @@
+// Imports
+import Utils from "Utils";
+
+//Solution
+export default function solve(fileName: string, quest: string): number {
+	const data = Utils.readData(fileName, quest);
+
+	return simulateBattle(parseInput(data));
+}
+
+type Point = { x: number; y: number };
+type Knight = Point & { id: string; value: number };
+type Target = Point & { hp: number };
+interface Battle {
+	targets: Target[];
+	knights: Knight[];
+}
+
+// Functions
+function parseInput(data: string): Battle {
+	const knights: Knight[] = [];
+	const targets: Target[] = [];
+	const grid = data.split("\n");
+
+	for (let y = 0; y < grid.length - 1; y++) {
+		for (let x = 0; x < grid[0].length; x++) {
+			const tile = grid[y][x];
+
+			if (tile === ".") continue;
+			else if (tile === "T") targets.push({ x, y, hp: 1 });
+			else if (tile === "H") targets.push({ x, y, hp: 2 });
+			else knights.push({ x, y, id: tile, value: tile.charCodeAt(0) - 64 });
+		}
+	}
+
+	return { knights, targets };
+}
+
+function simulateBattle({ knights, targets }: Battle) {
+	let score = 0;
+
+	targetLoop: while (targets.length) {
+		const target = targets.shift()!;
+
+		for (const { x, y, value } of knights) {
+			for (let power = target.x; power > 0; power--) {
+				const hit = collision({ x, y }, power, target);
+
+				if (hit) {
+					score += power * value * target.hp;
+					continue targetLoop;
+				}
+			}
+		}
+	}
+
+	return score;
+}
+
+function collision({ x, y }: Point, power: number, target: Point): boolean {
+	const path = [
+		[1, -1],
+		[1, 0],
+	];
+
+	for (const [dx, dy] of path) {
+		for (let i = 0; i < power; i++) {
+			[x, y] = [x + dx, y + dy];
+			if (x === target.x && y === target.y) {
+				return true;
+			}
+		}
+	}
+
+	while (x <= target.x && y <= target.y) {
+		[x, y] = [x + 1, y + 1];
+		if (x === target.x && y === target.y) {
+			return true;
+		}
+	}
+
+	return false;
+}
